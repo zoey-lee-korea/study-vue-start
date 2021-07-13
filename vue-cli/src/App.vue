51e50c12e50ed16431413b562f616a931753752c
zoey-lee-korea <wngpsid4014@gmail.com>
2021-07-13 18:41:50 +0900

    practice(vue-cli instances)

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
       <app-header></app-header>
+      <app-header 
+        v-bind:propsdata="name"
+        v-on:appFunction="realAppFunction"></app-header>
   </div>
 </template>
 
@@ -9,8 +12,18 @@ import AppHeader from './components/AppHeader.vue'
 // 위 코드는 사실상 var AppHeader = { template : <h1>Header</h1> } 와 동일하다
 
 export default {
+    data : function() {
+        return {
+            name : 'zoey'
+        }
+    },
     components : {
         'app-header' : AppHeader
+    },
+    methods : {
+        realAppFunction : function() {
+            console.log('app function performed')
+        }
     }
 }
 </script>
--- vue-cli/src/components/AppHeader.vue
+++ vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <h1>Header</h1>
+    <p>{{ propsdata }}</p>
+    <button v-on:click="sendEvent">click</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  props : [
+    'propsdata'
+  ],
+  methods : {
+    sendEvent : function() {
+      this.$emit('appFunction')
+    }
+  }
 }
 </script>
 
